5203f31b593bcdd3816f74156240f11f71f19c80
Christos Kitsanelis <sdi1200065@di.uoa.gr>
2016-12-01 20:44:15 +0200

    Evala ta arxeia

---
+++ exhash.c
@@ -0,0 +1,50 @@
+#include "BF.h"
+#include "exhash.h"
+
+
+int EH_CreateIndex(char *fileName, char* attrName, char attrType, int attrLength, int depth) {
+    /* Add your code here */
+
+    return -1;
+   
+}
+
+
+EH_info* EH_OpenIndex(char *fileName) {
+    /* Add your code here */
+
+    return -1;
+   
+} 
+
+
+
+int EH_CloseIndex(EH_info* header_info) {
+    /* Add your code here */
+
+    return -1;
+  
+}
+
+int EH_InsertEntry(EH_info* header_info, Record record) {
+    /* Add your code here */
+
+    return -1;
+   
+}
+
+
+int EH_GetAllEntries(EH_info header_info, void *value) {
+    /* Add your code here */
+
+    return -1;
+   
+}
+
+
+int HashStatistics(char* filename) {
+    /* Add your code here */
+
+    return -1;
+   
+}
---
+++ hash.c
@@ -0,0 +1,50 @@
+#include "BF.h"
+#include "hash.h"
+
+
+int HT_CreateIndex( char *fileName, char attrType, char* attrName, int attrLength, int buckets) {
+    /* Add your code here */
+
+    return -1;
+    
+}
+
+
+HT_info* HT_OpenIndex(char *fileName) {
+    /* Add your code here */
+
+    return -1;
+    
+} 
+
+
+int HT_CloseIndex( HT_info* header_info ) {
+    /* Add your code here */
+    
+    return -1;
+    
+}
+
+int HT_InsertEntry(HT_info header_info, Record record) {
+    /* Add your code here */
+    
+    return -1;
+    
+}
+
+
+
+int HT_GetAllEntries(HT_info header_info, void *value) {
+    /* Add your code here */
+
+    return -1;
+
+}
+
+
+int HashStatistics(char* filename) {
+    /* Add your code here */
+    
+    return -1;
+    
+}
